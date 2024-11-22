38ab191fd839d3099d5971a326b63855b95d9cd2
Sahaya <sahaya.stanislaus@tusimple.ai>
2024-11-22 11:50:23 +0530

    matrix python version

--- array_manipulation.py.py
+++ array_manipulation.py.py
@@ -2,4 +2,4 @@ def add_arrays(arr1, arr2):
     return [x + y for x, y in zip(arr1, arr2)]
 
 if __name__ == "__main__":
-    print(add_arrays([1, 2, 3], [4, 5, 6]))  # Output: [5, 7, 9]
+    print(add_arrays([1, 2, 3], [4, 5, 6]))
